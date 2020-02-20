2eb5c744573a90bf3e07f61c3c49a4ccae4fb66d
Ronald Adomako <ronaldadomako@icloud.com>
2020-02-20 17:02:30 -0500

    Problem 2

--- MicroServices_APIs/tasks-api-server/api.py
+++ MicroServices_APIs/tasks-api-server/api.py
@@ -50,20 +50,37 @@ USERS = [
 # E.g.,
 # @app.route("/users", methods=["GET"])
 
+# Problem 1
+
 @app.route("/users", methods=["GET"])
 def get_users():
     return jsonify(USERS)
-# Problem 1
-#@app.route('/user/<username>')
-def profile(username):
-    try:
-        return '{}\'s profile'.format(escape(username))
-    except:
-        return
+
+    
 # Problem 2
-
+@app.route("/users/<int:post_id>", methods=["GET"])
+def profile(post_id):
+    for user in USERS:
+        if post_id == user['id']:
+            return jsonify(user)
+    return Response(None,404)
 # Problem 3
-
+@app.route("/users", methods=["POST"])
+def add_user():
+    new_user = request.get_json()
+    if True: #All
+        #user id true
+        #user role true
+        #new user TRUE
+        
+        #append
+        response = jsonify(new_user)
+        response.status_code = 201
+        id_counter +=1
+    else:
+        response = Response(None, 422)
+    return response
+    
 # Problem 4
 
 # Problem 5
